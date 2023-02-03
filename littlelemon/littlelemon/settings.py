49a3706529e605a1afaaf67654d0dd5a87c364e7
Cris Vizcaino <cris.vizcaino@live.com>
2023-02-03 14:03:39 -0800

    Add

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -31,13 +31,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'restaurant',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "restaurant",
+    "rest_framework",
+    "rest_framework.authtoken",
+    "djoser",
 ]
 
 MIDDLEWARE = [
@@ -137,3 +140,22 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+REST_FRAMEWORK = {
+    
+    'DEFAULT_AUTHENTICATION_CLASSES':[
+        'rest_framework.authentication.TokenAuthentication',
+        "rest_framework.authentication.SessionAuthentication",
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+        "rest_framework.permissions.DjangoModelPermissions",
+    ],
+}
+
+
+DJOSER={
+    "USER_ID_FIELD":"username"
+}
--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,3 +1,41 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import generics, viewsets
+from rest_framework.permissions import IsAuthenticated
+
+from .models import Booking, Menu
+from  .serializers import BookingSerializer, MenuSerializer
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html', {})
+
+
+class MenuItemView(generics.ListCreateAPIView):
+	queryset = Menu.objects.all()
+	serializer_class = MenuSerializer
+
+	def get_permissions(self):
+		permission_classes = []
+		if self.request.method != 'GET':
+			permission_classes = [IsAuthenticated]
+		return [permission() for permission in permission_classes]
+
+
+
+
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+	queryset = Menu.objects.all()
+	serializer_class = MenuSerializer
+
+	def get_permissions(self):
+		permission_classes = []
+		if self.request.method != 'GET':
+			permission_classes = [IsAuthenticated]
+		return [permission() for permission in permission_classes]
+
+class BookingViewSet(viewsets.ModelViewSet):
+	queryset = Booking.objects.all()
+	serializer_class = BookingSerializer
+	permission_classes = [IsAuthenticated]
